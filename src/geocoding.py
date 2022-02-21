1bb15cf939e474ba0e40a319c6222d175347fe03
tbrittain <treybrittain@gmail.com>
2022-02-21 12:41:56 -0600

    Geocoding using OpenStreetMap

---
+++ src/geocoding.py
@@ -0,0 +1,53 @@
+import requests
+
+BASE_URL = "https://nominatim.openstreetmap.org/search"
+HEADERS = {'User-Agent': 'Distance Graph Model +https://github.com/tbrittain'}
+
+
+def geocode(**kwargs):
+    """
+    Geocode an address using the Nominatim API.
+    https://nominatim.org/release-docs/develop/api/Search/#parameters
+    :param q: query string - overrides all other parameters
+    :param street: street name
+    :param city: city name
+    :param state: state name
+    :param country: country name
+    :param postalcode: postal code
+    """
+
+    params = {
+        "format": "json",
+        "addressdetails": 1,
+        "countrycodes": "us",
+        "limit": 1
+    }
+
+    if "q" in kwargs:
+        params["q"] = kwargs["q"]
+    else:
+        for key, value in kwargs.items():
+            params[key] = value
+
+    resp = requests.get(url=BASE_URL, params=params, headers=HEADERS)
+    resp.raise_for_status()
+    return resp.json()[0]
+
+
+def geocode_coords(**kwargs):
+    """
+    Geocode an address using the Nominatim API and only return the coordinates.
+    https://nominatim.org/release-docs/develop/api/Search/#parameters
+    :param q: query string - overrides all other parameters
+    :param street: street name
+    :param city: city name
+    :param state: state name
+    :param country: country name
+    :param postalcode: postal code
+    """
+    resp = geocode(**kwargs)
+    return resp["lat"], resp["lon"]
+
+
+if __name__ == '__main__':
+    print(geocode_coords(q="1600 Pennsylvania Ave NW, Washington, DC 20500"))
